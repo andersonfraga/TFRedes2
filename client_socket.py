58fdb16cbf17ce9d155752180143afff2594d20f
Anderson Fraga <contato@andersonfraga.net>
2017-11-17 21:37:44 -0200

    Update client_socket.py

--- client_socket.py
+++ client_socket.py
@@ -1,11 +1,17 @@
+import os
+os.environ['http_proxy'] = ''
+
+import time
 import socket
 
-#create an INET, STREAMing socket
-s = socket.socket(
-    socket.AF_INET6, socket.SOCK_STREAM)
-#now connect to the web server on port 80
-# - the normal http port
+os.system('echo 1 > /proc/sys/net/ipv6/conf/all/forwarding')
+
+
+s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 s.connect(("2001:db8:0:1:d1:89ec:891:eba3", 1234))
 
 while True:
-	pass
+	print(s.recv(32))
+	time.sleep(1)
+
+s.close()
